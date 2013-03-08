4f66a0dd6d2b43bb9f5e7c9982cf7d39e49d354c
Heba El-Ayoty <hebaelayoty@odesk.com>
2013-03-08 11:16:22 -0800

    First skeleton of modules
    Contains basic interfaces of modules as per the spec document.

---
+++ Code/modules.py
@@ -0,0 +1,102 @@
+
+
+import os
+
+
+class EnergyPredictor:
+	"""stub"""
+	def __init__(self):
+		"""stub"""
+		pass
+
+	def generatePrimaryEnergyAvaialbility(self, start_date):
+		"""stub"""
+		pass
+
+
+class EnergyToElectricityConverter:
+	"""stub"""
+	def __init__(self, conversionFactor):
+		"""stub"""
+		self.conversionFactor = conversionFactor
+		pass
+
+	def generateElectiricityProduction(self):
+		"""stub"""
+		pass
+
+	def getElectricityProduction(self, start_date, end_date):
+		"""stub"""
+		pass
+
+
+class CashflowEstimator:
+	"""stub"""
+	def __init__(self):
+		"""stub"""
+		pass
+
+	def getRevenue(self, start_date, end_date):
+		"""stub"""
+		pass
+
+	def getCosts(self, start_date, end_date):
+		"""stub"""
+		pass
+
+	def calculateTaxes(self, start_date, end_date):
+		"""stub"""
+		pass
+
+	def getDebtPayment(self, start_date, end_date):
+		"""stub"""
+		pass
+
+	def calculateInterests(self):
+		"""stub"""
+		pass
+
+	def calculateFreeCashFlow(self):
+		"""stub"""
+		pass
+
+	def generateISandBS(self, start_date, end_date):
+		"""stub"""
+		pass
+
+	def calculateReturn(self):
+		"""stub"""
+		pass
+
+
+class EnvironmentalModule:
+	"""stub"""
+	def __init__(self):
+		"""stub"""
+		pass
+
+
+class SupportMechanismModule:
+	"""stub"""
+	def __init__(self):
+		"""stub"""
+		pass
+
+	def subsidyProduction(self):
+		"""stub"""
+		pass
+
+
+class RiskQuantifier:
+	"""stub"""
+	def __init__(self):
+		"""stub"""
+		pass
+
+	def outputIRRHistogram(self):
+		"""stub"""
+		pass
+
+	def outputIRRXYchart(self):
+		"""stub"""
+		pass
