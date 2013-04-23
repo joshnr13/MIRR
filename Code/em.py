fdbbff2561b252c77e719ce1e64e41e5c9f7a295
Sedov Anton <sedovanton@gmail.com>
2013-04-23 13:44:16 +0400

    Skelleton tm module

--- Code/em.py
+++ Code/em.py
@@ -6,106 +6,122 @@ import ConfigParser
 import csv
 import os
 
-def getConfig(filename='em_config.ini'):
-    """Reads module config file and makes variables global"""
-    config = ConfigParser.ConfigParser()
-    filepath = os.path.join(os.getcwd(), 'configs', filename)
-    config.read(filepath)
-    global mean, stdev
-    mean = config.getfloat('NormalDistribution', 'mean')
-    stdev = config.getfloat('NormalDistribution', 'stdev')
+class EnergyModule():
 
-def getRandomFactor():
-    """return random factor with normal distribution"""
-    result = random.gauss(mean, stdev)
-    return result
+    def __init__(self):
+        self.loadConfig()
+        self.loadInputs()
+        self.loadMainConfig()
 
-def getAvMonthInsolation(month):
-    """Returns average daily insolation in given month"""
-    filepath = os.path.join(os.getcwd(), 'inputs', 'em_input.txt')
-    result = numpy.genfromtxt(filepath, dtype=None, delimiter=';',  names=True)
-    return result[month-1]['Hopt']
+    def loadMainConfig(self, filename='main_config.ini'):
+        """Reads main config file """
+        config = ConfigParser.SafeConfigParser({'lifetime': 30, 'start_date': '2000/1/1', 'resolution': 10})
+        filepath = os.path.join(os.getcwd(), 'configs', filename)
+        config.read(filepath)
+        self.lifetime = config.getint('Main', 'lifetime')
+        self.resolution = config.getint('Main', 'resolution')
+        self.start_date = datetime.datetime.strptime(config.get('Main', 'start_date'), '%Y/%m/%d').date()
+        self.end_date = datetime.date(self.start_date.year + self.lifetime, self.start_date.month, self.start_date.day)
 
-def generatePrimaryEnergyAvaialbility(date):
-    """Parameters: start date
-    based on monthly averages creates daily data
-    """
-    """for each day of month:   .insulation = average monthly insolation for respective month * (random factor according to normal distribution)"""
-    date_month = date.month
-    av_insolation = getAvMonthInsolation(date_month)
-    return av_insolation * getRandomFactor()
+    def loadConfig(self, filename='em_config.ini'):
+        """Reads module config file"""
+        config = ConfigParser.ConfigParser()
+        filepath = os.path.join(os.getcwd(), 'configs', filename)
+        config.read(filepath)
+        self.mean = config.getfloat('NormalDistribution', 'mean')
+        self.stdev = config.getfloat('NormalDistribution', 'stdev')
 
-def generatePrimaryEnergyAvaialbility_range(start_date, end_date):
-    """Parameters: start date
-    based on monthly averages creates daily data
-    """
-    result = 0
-    while True:
-        if start_date > end_date:
-            return result
-        else:
-            result += generatePrimaryEnergyAvaialbility(start_date)
+    def getRandomFactor(self):
+        """return random factor with normal distribution"""
+        return random.gauss(self.mean, self.stdev)
+
+    def loadInputs(self):
+        """Loads inputs to memory"""
+        filepath = os.path.join(os.getcwd(), 'inputs', 'em_input.txt')
+        self.inputs = numpy.genfromtxt(filepath, dtype=None, delimiter=';',  names=True)
+
+    def getAvMonthInsolation(self, date):
+        """Returns average daily insolation in given date"""
+        return self.inputs[date.month-1]['Hopt']
+
+    def generatePrimaryEnergyAvaialbility(self, date):
+        """Parameters: start date
+        based on monthly averages creates daily data
+        """
+        """for each day of month:   .insulation = average monthly insolation for respective month * (random factor according to normal distribution)"""
+        return self.getAvMonthInsolation(date)* self.getRandomFactor()
+
+    def generatePrimaryEnergyAvaialbilityLifetime(self):
+        """return energy availability per each day in whole lifetime"""
+        lifetime_days = (self.end_date - self.start_date).days
+        return [self.generatePrimaryEnergyAvaialbility(self.start_date+datetime.timedelta(days=i)) for i in range(lifetime_days) ]
+
+
+    def getAccumulatedEnergy(self, start_date, end_date):
+        """
+        parameters: start date,end_date
+        return accumulated energy from start date(incl) till end date (incl)
+        """
+        result = 0
+        while start_date < end_date:
+            result += self.generatePrimaryEnergyAvaialbility(start_date)
             start_date += datetime.timedelta(days=1)
+        return result
 
-def getResolutionStartEnd(start_date, end_date, resolution):
-    """Returns list [start and end dates] using resolution"""
-    result = []
-    while True:
-        next_step_date = start_date + datetime.timedelta(days=resolution)
-        if start_date >= end_date:
-            break
-        if next_step_date <= end_date:
-            result.append((start_date, next_step_date))
+    def getResolutionStartEnd(self, start_date, end_date, resolution):
+        """Returns list [start , start_date + resolution] using resolution (days interval)"""
+        result = []
+        while start_date < end_date:
+            next_step_date = start_date + datetime.timedelta(days=resolution)
+            if next_step_date <= end_date:
+                result.append((start_date, next_step_date))
+                start_date = next_step_date
+            elif next_step_date > end_date:
+                result.append((start_date, end_date))
             start_date = next_step_date
-        elif next_step_date > end_date:
-            result.append((start_date, end_date))
-        start_date = next_step_date
-    return result
+        return result
 
+    def get_xy_values_for_plot(self, start_date, end_date, resolution):
+        """return x,y values for plotting step chart"""
 
-def get_xy_values_for_plot(start_date, end_date, resolution):
-    """return x,y values for plotting step chart"""
+        result = self.getResolutionStartEnd(start_date, end_date, resolution)
+        result.insert(0, (start_date, start_date))
+        y = []
 
-    result = getResolutionStartEnd(start_date, end_date, resolution)
-    result.insert(0, (start_date, start_date))
-    y = []
+        for start_period, end_period in result:
+            y.append(self.getAccumulatedEnergy(start_period, end_period))
 
-    for start_period, end_period in result:
-        y.append(generatePrimaryEnergyAvaialbility_range(start_period, end_period))
+        x = []
+        sm = 0
+        for r in result:
+            delta = (r[1] - r[0]).days
+            x.append(sm+delta)
+            sm += delta
 
-    x = []
-    sm = 0
-    for r in result:
-        delta = (r[1] - r[0]).days
-        x.append(sm+delta)
-        sm += delta
+        return x,y
 
-    return x,y
+    def outputPrimaryEnergy(self, start_date=None, end_date=None, resolution=None):
+        """Parameters: start_date; end_date; resolution
+           Makes a graph (x-axis displays time; y-axis= displays primary energy).
+           The minimum interval on the x-axis is set by resolution (integer number of days).
+           Values on the y-axis are sum of the energy in the time interval.
+        """
+        if start_date is  None:
+            start_date = self.start_date
+        if end_date is  None:
+            end_date = self.end_date
+        if resolution is  None:
+            resolution = self.resolution
 
+        x, y = self.get_xy_values_for_plot(start_date, end_date, resolution)
+        pylab.step(x, y)
+        pylab.show()
 
-def outputPrimaryEnergy(start_date, end_date, resolution):
-    """Parameters: start_date; end_date; resolution
-       Makes a graph (x-axis displays time; y-axis= displays primary energy).
-       The minimum interval on the x-axis is set by resolution (integer number of days).
-       Values on the y-axis are sum of the energy in the time interval.
-    """
-
-    datetime_delta = end_date - start_date
-    assert end_date > start_date
-    assert datetime_delta.days > resolution, "Difference between start and end date should be grater then resolution"
-    x, y = get_xy_values_for_plot(start_date, end_date, resolution)
-    pylab.step(x, y)
-    pylab.show()
-
-getConfig()
 
 if __name__ == '__main__':
-    start_date = datetime.date(2005, 1, 1)
-    end_date = datetime.date(2006, 12, 30)
-
-    #result = getResolutionStartEnd(start_date, end_date, 5)
-    #print getAvMonthInsolation(3)
-    #generatePrimaryEnergyAvaialbility(end_date)
-
-    outputPrimaryEnergy(start_date, end_date, 10)
+    start_date = datetime.date(2000, 1, 1)
+    end_date = datetime.date(2001, 12, 31)
+    em = EnergyModule()
+    #print em.generatePrimaryEnergyAvaialbilityLifetime()
+    em.outputPrimaryEnergy(start_date, end_date)
 
--- Code/tm.py
+++ Code/tm.py
@@ -1,17 +1,85 @@
+import pylab
+import datetime
+import ConfigParser
+import os
+from em import EnergyModule
 
-"""
-3.4.2	generateElectiricityProduction
-based on insolation generates electricity production values for each day
-produced electricity = insolation * energyConversionFactor
+class TechnologyModule():
+    def __init__(self, energy_module):
+        self.energy_module = energy_module
+        self.loadConfig()
+        self.loadMainConfig()
 
-3.4.3	getElectricityProduction
-Parameters: dateStart, dateEnd
-gives/reads the sum of the electricity produced in the specified period
-getElectiricityProduction = sum of electricity in kWh for each day for the selected period
+    def loadMainConfig(self, filename='main_config.ini'):
+        """Reads main config file """
+        config = ConfigParser.SafeConfigParser({'lifetime': 30, 'start_date': '2000/1/1', 'resolution': 10})
+        filepath = os.path.join(os.getcwd(), 'configs', filename)
+        config.read(filepath)
+        self.lifetime = config.getint('Main', 'lifetime')
+        self.resolution = config.getint('Main', 'resolution')
+        self.start_date = datetime.datetime.strptime(config.get('Main', 'start_date'), '%Y/%m/%d').date()
+        self.end_date = datetime.date(self.start_date.year + self.lifetime, self.start_date.month, self.start_date.day)
 
-3.4.4	outputElectricityProduction
-Parameters: start_date; end_date; resolution
-Makes a graph (x-axis displays time; y-axis= displays electricity produced). The minimum interval on the x-axis is set by resolution (integer number of days).  Values on the y-axis are sum of the electricity produced in the time interval.
+    def loadConfig(self, filename='tm_config.ini'):
+        """Reads module config file"""
+        config = ConfigParser.ConfigParser()
+        filepath = os.path.join(os.getcwd(), 'configs', filename)
+        config.read(filepath)
+        self.electr_conv_factor = config.getfloat('Electricity', 'ConversionFactor')
+
+    def generateElectiricityProduction(self, date):
+        """based on insolation generates electricity production values for each day
+        produced electricity = insolation * energyConversionFactor"""
+        insolation = self.energy_module.generatePrimaryEnergyAvaialbility(date)
+        return insolation * self.electr_conv_factor
+
+    def getElectricityProduction(self, date_start, date_end):
+        """return sum of electricity in kWh for each day for the selected period"""
+        duration_days = (date_end - date_start).days
+        return sum([self.generateElectiricityProduction(self.start_date+datetime.timedelta(days=i)) for i in range(duration_days) ])
 
 
-"""
+    def get_xy_values_for_plot(self, start_date, end_date, resolution):
+        """return x,y values for plotting step chart"""
+
+        result = self.energy_module.getResolutionStartEnd(start_date, end_date, resolution)
+        y = []
+
+        for start_period, end_period in result:
+            y.append(self.getElectricityProduction(start_period, end_period))
+
+        x = []
+        sm = 0
+        for r in result:
+            delta = (r[1] - r[0]).days
+            x.append(sm+delta)
+            sm += delta
+
+        return x,y
+
+    def outputElectricityProduction(self, start_date=None, end_date=None, resolution=None):
+        """Makes a graph (x-axis displays time; y-axis= displays electricity produced).
+        The minimum interval on the x-axis is set by resolution (integer number of days).
+        Values on the y-axis are sum of the electricity produced in the time interval.
+        """
+        if start_date is  None:
+            start_date = self.start_date
+        if end_date is  None:
+            end_date = self.end_date
+        if resolution is  None:
+            resolution = self.resolution
+
+        x, y = self.get_xy_values_for_plot(start_date, end_date, resolution)
+        pylab.step(x, y)
+        pylab.show()
+        print x, y
+
+
+
+if __name__ == '__main__':
+    start_date = datetime.date(2000, 1, 1)
+    end_date = datetime.date(2001, 12, 31)
+    em = EnergyModule()
+    #print em.generatePrimaryEnergyAvaialbilityLifetime()
+    tm = TechnologyModule(em)
+    tm.outputElectricityProduction(start_date, end_date)
