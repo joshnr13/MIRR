2ee2d79e4d1c658126d959f6fbaabf630ad06fe3
Sedov Anton <sedovanton@gmail.com>
2013-07-23 20:18:34 +0400

    add temporary new price generation module

---
+++ Code/try_new_price_generation.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pylab
+
+"""
+We use the Mean-reverting jump diffusion model (formula 4 - see comments)
+
+As inputs we use:
+- starting S - config value - 120 EUR/MWh
+ - k -  for now use a uniform value - is a config value and set it to 1
+- theta -  config value - 0,3 EUR/h
+- lambda - config value - 0,02
+-  sigma -  for now use a uniform value - is a config value and set it to 0,1
+- J use the values in table 2 - see comments
+- dq - is a possion process - use lambda from table 2
+
+Calculate the values for 30 year 10 times and display the values on a graph. If correct should look more or less like the picture attached below.
+"""
+S0 = 120  #EUR/MWh
+k = 1
+theta = 0.3  #EUR/h
+Lambda = 0.02
+sigma = 0.1
+y = 0  #is the annual escalation factor
+J =  154.85# mu from table 2
+delta_q = 24.26  #lambda from table 2
+
+T = 30  #years
+dt = 1.0 / 365  #1day
+N = int(round(T/dt))  #number of periods
+
+def delta_brownian():
+    """Calculated delta between 2 values with normal distribution"""
+    two_randoms = np.random.standard_normal(size = 2)
+    return  np.sqrt(dt) * (two_randoms[1] - two_randoms[0])
+
+def calc_price_delta(prev_price):
+    """Calculated delta price based on @prev_price"""
+    delta_Z = delta_brownian()
+    delta_price = k * (theta * (1 + y) - prev_price ) * dt + sigma * delta_Z + (J - prev_price) * delta_q
+    return  delta_price
+
+def calc_price_for_period(prev_price):
+    """Calculate delta price for whole period"""
+    result = []
+    for i in range(N):
+        price = calc_price_delta(prev_price)
+        prev_price = price
+        result.append(price)
+    return  result
+
+price = calc_price_for_period(S0)
+pylab.plot(price)
+pylab.show()
