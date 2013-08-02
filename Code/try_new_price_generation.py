44ab0bdbcfcb55dc58e738f5ec15995b04c1b231
Borut Del Fabbro <borut.delfabbro@gmail.com>
2013-08-02 23:32:05 +0200

    minor corrections to price calculation

--- Code/try_new_price_generation.py
+++ Code/try_new_price_generation.py
@@ -15,11 +15,11 @@ As inputs we use:
 Calculate the values for 30 year 10 times and display the values on a graph. If correct should look more or less like the picture attached below.
 """
 
-S0 = 150  #EUR/MWh
-k = 1
-theta = 150  #EUR/MWh
+S0 = 70  #EUR/MWh
+k = 5
+theta = 70  #EUR/MWh
 Lambda = 24  # Lambda for the Poisson process used for price jumps
-sigma = 2
+sigma = 0.5
 y = 0.02  #is the annual escalation factor
 delta_q = 0.5  #random variable with Poisson distribution with lambda 24.26
 #J = calc_J()
@@ -65,7 +65,7 @@ class Poisson_step():
 
 def calc_J():
     """Calcultation of J as random with mean=loc and std=scale"""
-    return  np.random.normal(loc=150, scale=80)  #loc means - mean, scale -std
+    return  np.random.normal(loc=0, scale=1)  #loc means - mean, scale -std
 
 def poisson_distribution_value(lam=Lambda, size=None):
     """return  Poisson disribution with @lam
@@ -81,12 +81,12 @@ def delta_brownian():
 
 def calc_price_delta(prev_price, iteration_no):
     """Calculated delta price based on @prev_price"""
-    delta_Z = delta_brownian()
+    delta_Z = np.random.normal(loc=0, scale=0.4)
     J = calc_J()
 
     delta_q = poisson_steps.get_delta(iteration_no)
 
-    delta_price = k * (theta * 24* (1 + y*iteration_no/365)- prev_price) * dt + sigma * delta_Z + (J*(1 + y*iteration_no/365) - prev_price) * delta_q
+    delta_price = k * (theta * (1 + y*iteration_no/365)- prev_price) * dt + sigma * delta_Z + (J*(1 + y*iteration_no/365)) * delta_q
     return  delta_price
 
 def calc_price_for_period(prev_price):
