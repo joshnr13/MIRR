aaadd8ac85be36b6672d504ecdd4b15803b22e87
Jure Slak <jure.slak@gmail.com>
2015-08-03 12:42:17 +0200

    Add lognormal mean reversion price generation.

--- Code/ecm.py
+++ Code/ecm.py
@@ -5,7 +5,7 @@ import datetime
 from dateutil.relativedelta import relativedelta
 import numpy as np
 import random
-from math import exp
+from math import exp, log
 
 from rm import calcStatistics
 from tm import TechnologyModule
@@ -71,47 +71,29 @@ class ElectricityMarketPriceSimulation(EconomicModuleConfigReader):
         simulation_result = {"simulation_no": simulation_no, "data": days_dict, "stats": stats}
         return simulation_result
 
-    def calcJump(self):
-        """Calcultation of J (normaly distibuted Jump size) as random with mean=loc and std=scale
-        loc means - mean, scale -std """
-        return np.random.normal(loc=self.jump_size_average, scale=self.jump_size_std)
-
-    def calcPriceDeltaNoJump(self, prev_price, iteration_no, theta):
+    def calcPriceLogDeltaNoJump(self, prev_price_log, theta_log):
         """Calculates delta price (dp) based on @prev_price without a price jump"""
         #delta_Z = np.random.normal(loc=0, scale=0.9)  #random value distribution
-        delta_Z = random.choice([-1, 1]) * exp(np.random.normal(loc=-0.85, scale=1.1) )
+        delta_Z = np.random.normal(loc=5.1725e-04, scale=0.023594)
 
-        delta_price = self.k * (theta - prev_price) + delta_Z
-        return  delta_price
-
-    def calcPriceDeltaWithJump(self, prev_price, iteration_no, theta):
-        """Calculated delta price (dp) based on @prev_price with a price jump"""
-
-        J = self.calcJump() #calculate jump
-
-        delta_price = self.calcPriceDeltaNoJump(prev_price, iteration_no, theta)  + J  #add jump to delta price
-        return  delta_price
+        delta_price_log = self.k * (theta_log - prev_price_log) + delta_Z
+        return  delta_price_log
 
     def calcPriceWholePeriod(self, start_price):
         """Calculate price for whole period from start date to end - return  list with prices for all project days"""
         result = []
-        date_next_jump = int(random.expovariate(self.Lambda)) #calculate the first date of price jump
         y = self.makeInterannualVariabilityY()
         theta = self.theta
 
-        prev_price = start_price
+        prev_price_log = log(start_price)
 
         for i, date in enumerate(self.period):
             if date.weekday() < 5:
-                if i == date_next_jump:
-                    price = prev_price + self.calcPriceDeltaWithJump(prev_price, i+1, theta)
-                    date_next_jump += int(random.expovariate(self.Lambda)) + 1 # add one if interval is 0
-                else:
-                    price = prev_price + self.calcPriceDeltaNoJump(prev_price, i+1, theta)
+                price_log = prev_price_log + self.calcPriceLogDeltaNoJump(prev_price_log, i+1, log(theta))
 
             theta = theta * (1 + y/365)
-            prev_price = price
-            result.append(price)
+            prev_price_log = price_log
+            result.append(exp(price_log))
             if isLastDayYear(date):  # recalculate y each new year
                 y = self.makeInterannualVariabilityY()
 
